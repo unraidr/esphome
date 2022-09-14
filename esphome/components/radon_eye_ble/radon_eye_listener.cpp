b29f84d56fc6876b493e91bad221633a39f0397a
unraidr <98129010+unraidr@users.noreply.github.com>
2022-09-14 16:49:45 -0600

    radon eye listener shows newer firmware units

--- esphome/components/radon_eye_ble/radon_eye_listener.cpp
+++ esphome/components/radon_eye_ble/radon_eye_listener.cpp
@@ -11,8 +11,12 @@ static const char *const TAG = "radon_eye_ble";
 bool RadonEyeListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
   if (not device.get_name().empty()) {
     if (device.get_name().rfind("FR:R20:SN", 0) == 0) {
-      // This is an RD200, I think
-      ESP_LOGD(TAG, "Found Radon Eye RD200 device Name: %s (MAC: %s)", device.get_name().c_str(),
+      // this is an older firmware RD200
+      ESP_LOGD(TAG, "Found Radon Eye RD200 Device (V0) Name: %s (MAC: %s)", device.get_name().c_str(),
+               device.address_str().c_str());
+    } else if (device.get_name().rfind("FR:RU", 0) == 0) {
+      // this is a newer firmware RD200
+      ESP_LOGD(TAG, "Found Radon Eye RD200 Device (V1) Name: %s (MAC: %s)", device.get_name().c_str(),
                device.address_str().c_str());
     }
   }
